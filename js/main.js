aea850d60156b4274b08b5ec306bd3d079ba9626
RNCST <rk51320928@gmail.com>
2021-09-16 15:20:33 +0900

    21/09/16 promo reward fin youtube ing

--- js/main.js
+++ js/main.js
@@ -30,8 +30,8 @@ const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(
     // _.throttle(function, function이 실행되는 시간의 단위 ms)
     function() {
-        console.log('scroll!');
-        console.log(window.scrollY);
+        // console.log('scroll!');
+        // console.log(window.scrollY);
         if(window.scrollY> 500){
             //badge hide
             // badgeEl.style.display = 'none'
@@ -71,3 +71,40 @@ new Swiper('.notice-line .swiper-container', {
     autoplay: true, // 자동 재생 여부
     loop: true // 반복 재생 여부
   })
+
+
+new Swiper('.promotion .swiper-container',{
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 10,//사이여백
+    centeredSlides: true, //1번 슬라이드가 가운데에 보이기 
+    loop: true,
+    autoplay: {
+        delay: 5000 //ms
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable:true,
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next',
+    }
+    
+
+
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',function() {
+    isHidePromotion = !isHidePromotion
+    // on off
+    if(isHidePromotion){
+        promotionEl.classList.add('hide');
+    } else {
+        promotionEl.classList.remove('hide');
+    }
+});
